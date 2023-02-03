8e13a07d8fbbb1b22c25380f82f5b38f69baed30
wnduddl30 <leejy3022@naver.com>
2023-02-03 22:46:31 +0900

    OCP, DIP 위반 문제점

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -2,6 +2,7 @@ package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.FixDiscountPolicy;
+import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
@@ -9,7 +10,12 @@ import hello.core.member.MemoryMemberRepository;
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository = new MemoryMemberRepository();
-    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
+    //private final DiscountPolicy discountPolicy = new RateDiscountPolicy();
+    // -> 변경하려면 코드를 수정해야 함 -> OCP, DIP 위반
+
+    private DiscountPolicy discountPolicy; //구체화에 의존하지 않고 추상화(인터페이스)에만 의존
+
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {

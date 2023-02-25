89c20779b44d15b1143b21d91e2d277de76d3d8e
wnduddl30 <leejy3022@naver.com>
2023-02-26 00:51:54 +0900

    조회 빈이 2개 이상일 때 해결법 - @Qualifier

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,10 +5,11 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
+//@RequiredArgsConstructor
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
@@ -54,14 +55,12 @@ public class OrderServiceImpl implements OrderService{
     }
      */
 
-/*
+
     @Autowired  //생성자 주입 : 생성자가 1개일 때는 @Autowired 생략 가능
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
- */
-
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {

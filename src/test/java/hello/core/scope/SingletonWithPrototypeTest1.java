12b1e45269c105f313f1f5ed97de19d727cec461
wnduddl30 <leejy3022@naver.com>
2023-03-02 20:14:10 +0900

    프로토타입 스코프 - 싱글톤 빈과 함께 사용시ObjectProvider로 문제 해결

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -2,6 +2,7 @@ package hello.core.scope;
 
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
@@ -34,20 +35,19 @@ public class SingletonWithPrototypeTest1 {
 
         ClientBean clientBean2 = ac.getBean(ClientBean.class);
         int count2 = clientBean2.logic();
-        assertThat(count2).isEqualTo(2);
+        assertThat(count2).isEqualTo(1);
     }
 
     @Scope("singleton")
     @Component
     static class ClientBean{
-        private final PrototypeBean prototypeBean;
-
         @Autowired
-        public ClientBean(PrototypeBean prototypeBean) {
-            this.prototypeBean = prototypeBean;
-        }
-
+        private ObjectProvider<PrototypeBean> prototypeBeanProvider; //ObjectFactory로 바꿔도 됨
+        //ObjectProvider는 스프링이 자동으로 만들어서 주입해줌
         public int logic(){
+            //getObject()을 하면 이때 스프링 컨테이너에서 PrototypeBean을 찾아서 반환해줌 -> ac한테 직접 찾는게 아니라 찾아주는 기능만 제공해줌
+            //스츠링 컨테이너 조회하는데 직접 하는게 아니라 대신해주는 대리자 역할
+            PrototypeBean prototypeBean = prototypeBeanProvider.getObject();
             prototypeBean.addCount();
             int count = prototypeBean.getCount();
             return count;
